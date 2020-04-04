ca8e7bcbf44aa2659f547d950957a6dc34250acd
Ariejan de Vroom <ariejan@devroom.io>
2020-04-04 15:02:46 +0200

    More layout and renaming

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeScreen extends StatefulWidget {
-//  HomeScreen({Key key, this.title}) : super(key: key);
-
-
   final String _title = "Space Engineer";
 
   @override
@@ -11,68 +9,80 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  int _resources = 0;
+  int _astroids = 1;
 
-  void _incrementCounter() {
+  void _mineAstroids() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _resources += _randomMiningYield(_astroids);
     });
+  }
+
+  int _randomMiningYield(int astroids) {
+    return astroids * 10;
+  }
+
+  Widget _buildIncrementButton(BuildContext context) {
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: FlatButton(
+        onPressed: _mineAstroids,
+        padding: const EdgeInsets.fromLTRB(24.0, 24.0, 24.0, 36.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              child: FaIcon(FontAwesomeIcons.sun, size: 28.0),
+              padding: const EdgeInsets.only(right: 8.0),
+            ),
+            Text(
+              _astroids == 1 ? "Mine 1 astroid" : "Mine $_astroids astroids",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+              )
+            ),
+          ],
+        ),
+        color: Colors.amber,
+        textColor: Colors.black,
+      )
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget._title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.fromLTRB(16.0, 8.0, 16.0, 8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: <Widget>[
+                Container(
+                  child: FaIcon(FontAwesomeIcons.atom, size: 16.0),
+                  padding: const EdgeInsets.only(right: 8.0),
+                ),
+                Text(
+                  "$_resources",
+                  style: TextStyle(
+                    fontSize: 18.0,
+                  )
+                )
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+          ),
+          Expanded(
+            flex: 10,
+            child: Text("TODO"),
+          ),
+          _buildIncrementButton(context),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:space_engineer/main.dart';
 
 void main() {
-  testWidgets('Counter increment score by 1', (WidgetTester tester) async {
+  testWidgets('Counter increment score by 10', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(SpaceEngineerApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('10'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text("Mine 1 astroid"));
     await tester.pump();
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    expect(find.text('10'), findsOneWidget);
   });
 }
