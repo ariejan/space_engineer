1e7e6fb128912332577a12480576d6ce3dd29ea0
Ariejan de Vroom <ariejan@devroom.io>
2020-04-04 14:14:32 +0200

    Set color scheme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,18 +1,10 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  HomeScreen({Key key, this.title}) : super(key: key);
+//  HomeScreen({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  final String _title = "Space Engineer";
 
   @override
   _HomeScreenState createState() => _HomeScreenState();
@@ -44,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(widget._title),
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:space_engineer/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Counter increment score by 1', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(SpaceEngineerApp());
 
