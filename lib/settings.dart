b9dd2bbe2aa0e83dee794dcc5886ae6cc49f8082
Ariejan de Vroom <ariejan@devroom.io>
2020-04-07 15:51:39 +0200

    Tweak cooldown / fps

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,5 +1,5 @@
 // Timer for handling cooldowns, about 60fps for smooth indicators
-const int timerMs = 16;
+const int timerMs = 100;
 
 // All cooldowns are in ms.
-const int miningCooldown = 1500;
+const int miningCooldown = 3000;
